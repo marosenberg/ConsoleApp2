e20cd2e1fdd36f9b0d4f7b1633759029fd4e749e
Mark Rosenberg <mrosenberg@prscholas.org>
2021-04-01 12:03:18 -0500

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // double[] ar = new double[] { 5, 4, 3, 2, 1 };
+            myMethod("Me", 1,2,3,4,5);
+            Console.ReadKey();
+        }
+
+        static void myMethod(string name, params double[] d)
+        {
+            foreach (var item in d)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
